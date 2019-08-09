992553fabd71722449badfa9086e082aaf900dab
CMIS User <Solomon2590-cmis>
2019-08-09 11:44:15 +0700

    initial commit

---
+++ Unit1/PrintingStrings.java
@@ -0,0 +1,30 @@
+
+/**
+ * Write a description of class PrintingStrings here.
+ *
+ * @author (Solomon)
+ * @version (0.1)
+ */
+public class PrintingStrings
+{
+    public static void main(String[] agrv){
+        System.out.println("Hello world");
+        System.out.println("I am Handsome");
+        
+        //string literal
+        String thisisstring = "Haha"; 
+        //string instance 
+        String anotherString = new String("Another haha"); 
+        
+        System.out.println(thisisstring);
+        System.out.println(anotherString);
+        
+        //declaration 
+        String myname;
+        //definitin 
+        myname = "Solomon";
+        
+        String error;
+        
+    }
+}
