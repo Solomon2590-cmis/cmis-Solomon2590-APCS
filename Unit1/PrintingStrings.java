94d5a43afc3a7ded870070e3f06e2c47197b4496
CMIS User <solomon2590@cmis.ac.th>
2019-08-13 14:21:42 +0700

    Trying to find out what went wrong while commit

--- Unit1/PrintingStrings.java
+++ Unit1/PrintingStrings.java
@@ -1,4 +1,4 @@
-
+import java.util.Scanner; 
 /**
  * Write a description of class PrintingStrings here.
  *
@@ -8,26 +8,35 @@
 public class PrintingStrings
 {
     public static void main(String[] agrv){
+        //Scanner is type of the variable
+        //System.in is where the input is going to be from
+        Scanner input = new Scanner(System.in);
+        //input.nextLine is telling the computer to wait for your input
+        String inputTest = input.nextLine();
+        
+        System.out.format("Hello: %s\n", inputTest); 
+        
         System.out.println("Hello world");
         System.out.println("I am Handsome");
         
         //string literal
-        String thisisstring = "Haha"; 
+        String thisIsString = "Haha"; 
         //string instance 
         String anotherString = new String("Another haha"); 
         
-        System.out.println(thisisstring);
+        System.out.print(thisIsString + "\n");
         System.out.println(anotherString);
         
         //declaration 
-        String myname;
+        String myName;
         //definitin 
-        myname = "Solomon";
+        myName = "Solomon";
         
         String error;
         error = "there's nothing to change";
         
-        Integer itisinteger = new Integer(2590);
-        System.out.print(itisinteger);
+        Integer itIsInteger = new Integer(2590);
+        System.out.print(itIsInteger);
+        System.out.print("change something");
     }
 }
