177905df6b225c44c0fb6f22ad93d688a2118b90
Solomon2590-cmis <solomon2590@cmis.ac.th>
2019-09-30 09:56:42 +0700

    finsiehd class

--- Unit2/Square.java
+++ Unit2/Square.java
@@ -1,23 +1,22 @@
 class Square{
     private double sideLength;
     private String color;
-    private boolean realOrFake;
     
-    public Square (int sideLength, String color, boolean realOrFake){
+    public Square (int sideLength, String color){
         this.sideLength = sideLength;
         this. color = color;
     }
     public Square(String color){
         this.color = color;
     }
-    public Square(int sideLength){
+    public Square(double sideLength){
         this.sideLength = sideLength; 
     }
     public double getSideLength(){
         return sideLength;
     }
     public void setSideLength(int newSideLength){
-        sideLength = newSideLength;
+        this.sideLength = newSideLength;
     }
     public String getColor(){
         return color;
@@ -25,22 +24,23 @@ class Square{
     public void setColor(String newColor){
         color = newColor;
     }
-    public boolean getRealOrFake(){
-        return realOrFake;
-    }
-    public void setRealOrFake(boolean newRealOrFake){
-        realOrFake = newRealOrFake;
-    }
     public double getArea(){
         return sideLength*sideLength;
     }
     public void setArea(double newArea){
         sideLength = Math.sqrt(newArea);
     }
-    public double getDiagonal(){
-        return Math.sqrt(Math.pow(sideLength,2),Math.pow(sideLength,2));
+    public Square subtract(Square other){
+        double aArea = this.getArea();
+        double bArea = other.getArea();
+        double positive = Math.max(aArea,bArea);
+        double negative = Math.min(aArea,bArea);
+        double diff = positive - negative;
+        Square result = new Square(Math.sqrt(diff));
+        return result; 
     }
-       public String toString(){
-        return String.format("Moses's sidelength:%d\nColor:%s",sideLength,color);
+    public String toString(){
+        return String.format("Moses's sidelength:%f\nColor:%s",this.sideLength,this.color);
     }
+    
 }
--- Unit2/SquareTester.java
+++ Unit2/SquareTester.java
@@ -2,10 +2,10 @@
 public class SquareTester
 {
    public static void main(String [] argv){
-       Square squareA = new Square(1,"red",false);
+       Square squareA = new Square(1,"red");
        System.out.println(squareA);
-       Square squareb = new Square(0, "null", true);
-       System.out.println(squareb);
+       Square squareb = new Square(0, "null");
+       System.out.println(squareb); 
     }
 }
 
