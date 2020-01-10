0e1aa3b8a1d7b1c2ed6a6fee4fc974266eec9bda
Solomon2590-cmis <solomon2590@cmis.ac.th>
2020-01-10 09:56:27 +0700

    finished first questoin and doing 2nd one

--- Unit4/ForLoopProblems.java
+++ Unit4/ForLoopProblems.java
@@ -10,12 +10,46 @@ public class ForLoopProblems
     public static String table(int rows, int columns, int operation){
         String out = "";
         for(int columnss = 0; columnss < columns; columnss++) {
-            for(int rowss = 0; rowss < rows; rows++){
+            for(int rowss = 0; rowss < rows; rowss++){
             if (operation == 0){
-                out += columnss+rowss+"\t";
-                
+                out += (columnss + rowss)+ "\t";
             }
+            else if (operation == 1){
+                out += (columnss - rowss) + "\t";
+            }
+            else if (operation == 2){
+                out += (columnss * rowss) + "\t";
+            }
+            else if (operation == 3){
+                if ( rowss == 0){
+                    out += "error\t";
+                }
+                else{
+                    out += (columnss / rowss) + "\t";
+                }
+            }
+            else if (operation == 4){
+                out += Math.pow(columnss, rowss) + "\t";
+            }
+            else {
+                return "error";
             }
         }
+        out += "\n";
+    }
+     System.out.println(out);
+    return out;
+}
+public static String map(int height, int width, String symbols){
+    String out = "";
+    int random = ((int)(Math.random() * 10)) % symbols.length();
+    for (int heightt = 0; heightt < height; heightt++){
+        for (int widthh = 0; widthh < width; widthh++){
+            out += symbols.substring(((int)(Math.random() * 10)) % symbols.length()) + "\t";
+        }
+        out += "\n";
+    }
+    System.out.println(out);
+    return out;
 }
 }
