0b1a809169fc996372d829fccaee75be2841e436
Solomon2590-cmis <solomon2590@cmis.ac.th>
2020-01-23 11:53:19 +0700

    didn't finish much. finished the second question.

--- Unit5/ArraysProblemSet.java
+++ Unit5/ArraysProblemSet.java
@@ -11,4 +11,16 @@ public static int max(int[] array){
     }
     return greater;
 }
+public static int minIdx(int[] array){
+    int len = array.length;
+    int bit = array[0];
+    int re = 0;
+    for(int lenn = 1; lenn < len; lenn++){
+        if(Math.min(array[lenn],bit)==array[lenn]){
+            bit = array[lenn];
+            re = lenn;
+        }
+    }
+    return re;
+}
 }
