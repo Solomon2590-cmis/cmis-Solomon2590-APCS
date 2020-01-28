a59b9228269fce8eb1ee979938a508598beb6552
Solomon2590-cmis <solomon2590@cmis.ac.th>
2020-01-28 09:54:29 +0700

    finished till interleave

--- Unit5/ArraysProblemSet.java
+++ Unit5/ArraysProblemSet.java
@@ -23,4 +23,104 @@ public static int minIdx(int[] array){
     }
     return re;
 }
+public static int sum(int[] array){
+    int re = 0;
+    for(int i = 0; i < array.length; i++){
+        re += array[i];
+    }
+    return re;
 }
+public static double avg(int[] array){
+    int re = 0;
+    for(int i = 0; i < array.length; i++){
+        re += array[i];
+    }
+    re = re/(array.length);
+    return re;
+}
+public static boolean allGreater(int[] array,int threshold){
+    boolean re = true;
+    for(int i = 0; i < array.length; i++){
+        if(array[i]>threshold){
+            re = false;
+        }
+    }
+    return re; 
+}
+public static boolean oneGreater(int[] array,int threshold){
+    boolean re = false;
+    int hi = 0;
+    for(int i = 0; i < array.length; i++){
+        if(array[i]>threshold && hi == 0){
+            re = true;
+            hi++;
+        }
+        if(array[i]>threshold && hi ==1){
+            re = false;
+        }
+    }
+    return re; 
+}
+public static int sumOfLessThan(int[] array, int threshold){
+    int re = 0;
+    for(int i = 0; i < array.length; i++){
+        if(array[i]<threshold){
+            re += array[i];
+        }
+    }
+    return re;
+}
+public static int greatestPairIdx(int[] array){
+    int re = 0;
+    int gsum = 0;
+    for(int i = 0; i < array.length-1; i++){
+        gsum = Math.max(gsum,array[i]+array[i+1]);
+        if(gsum == array[i]+array[i+1]){
+            re = i;
+        }
+    }
+    return re; 
+}
+public static int[] reverse(int[] array){
+    int[] reverse;
+    reverse = new int[array.length];
+    int reversenum = 0;
+    for(int i = (array.length)-1; i >= 0; i--){
+        reverse[reversenum] = array[i];
+        reversenum++;
+    }
+    return reverse;
+}
+public static int[] shiftLeft(int[] array, int distance){
+    int[] re = new int[array.length];
+    int index = 0;
+    int addedval = 0;
+    for(int i = 0; i < array.length; i++){
+        if( i+distance > array.length-1){
+            addedval = i+distance- ((array.length));
+            re[i] = array[addedval];
+        }
+        else{
+            re[i] = array[i+distance];
+        }
+    }
+    return re;
+}
+public static int[] sifthRIght(int[] array, int distance){
+     int[] re = new int[array.length];
+    int index = 0;
+    int addedval = 0;
+    for(int i = (array.length)-1; i >= 0; i--){{
+        if(i-distance < 0){
+            addedval = i-distance + array.length;
+            re[i] = array[addedval];
+        }
+        else{
+            re[i] = array[i-distance];
+        }
+    }
+    }
+    return re;
+}
+}
+
