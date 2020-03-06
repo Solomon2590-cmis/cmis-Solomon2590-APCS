e778cd243b550b0c59bca2a41073c7ca0f9b45ae
Solomon2590-cmis <solomon2590@cmis.ac.th>
2020-03-06 14:33:25 +0700

    finished the game of life

--- Unit8/GameOfLife.java
+++ Unit8/GameOfLife.java
@@ -8,15 +8,33 @@ public class GameOfLife
      * 4. Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
      */
     public static void main(String[] argv){
-        int[][] board = {{0,0,1,0,0},{0,0,0,0,0},{0,0,1,0,1}};
+        int[][] board = {{0,1,1,0,0,0,0,0},
+                         {0,0,1,0,0,0,0,0},
+                         {0,0,0,0,0,0,0,0},
+                         {0,0,0,0,0,0,0,0},
+                         {0,0,0,0,0,0,0,0},
+                         {0,0,0,0,0,0,0,0},
+                         {0,0,0,0,0,0,0,0}};
+            
         System.out.println(printNextGen(nextGeneration(board)));
+        int[][] b= nextGeneration(board);
+        for(int i = 0; i < 20; i++){
+            System.out.println(printNextGen(b));
+            System.out.println(("____________________" + i));
+            b = nextGeneration(b);
+        }
     }   
     public static String printNextGen(int[][] board){
         String output = "";
         for(int col = 0; col < board.length; col++){
             for(int row = 0; row < board[0].length; row++){
+                if(board[col][row] == 1){
                 output+= board[col][row] + " ";
             }
+                else{
+                    output+= "  ";
+                }
+        }
             output+= "\n";
         }
         return output;
@@ -34,38 +52,37 @@ public class GameOfLife
             for(int row = 0; row < board[0].length; row++){
                 for(int compareWhich = 0; compareWhich < 8; compareWhich++){
                     if(compareWhich == 0 && (col-1!=-1 && row-1!=-1)){
-                        nearl(board, col-1, row-1, near);
+                        near = nearl(board, col-1, row-1, near);
                         }
                     if(compareWhich == 1 && (col-1!=-1)){
-                        nearl(board, col-1, row, near);
+                        near = nearl(board, col-1, row, near);
                     }
                     if(compareWhich == 2 && (col-1!=-1 && row+1!= board[0].length)){
-                        nearl(board, col-1, row+1, near);
+                        near = nearl(board, col-1, row+1, near);
                     }
                     if(compareWhich == 3 && ( row-1 != -1)){
-                        nearl(board, col, row-1, near);
+                        near = nearl(board, col, row-1, near);
                     }
                     if(compareWhich == 4 && (row+1 != board[0].length)){
-                        nearl(board, col, row+1, near);
+                        near = nearl(board, col, row+1, near);
                     }
                     if(compareWhich == 5 && (col+1 != board.length && row-1 != -1)){
-                        nearl(board, col+1, row-1, near);
+                        near = nearl(board, col+1, row-1, near);
                     }
                     if(compareWhich == 6 && (col+1 != board.length)){
-                        nearl(board, col+1, row, near);
+                        near = nearl(board, col+1, row, near);
                     }
                     if(compareWhich == 7 && (col+1 != board.length && row+1 != board[0].length)){
-                        nearl(board, col+1, row+1, near);
+                        near = nearl(board, col+1, row+1, near);
                     }
                     }
-                System.out.println(near);
-                if(near ==1){
+                if(near ==1&& board[col][row] == 1){
                     nexG[col][row] = 0;
                 }
                 else if(near > 1 && near < 4 && board[col][row] == 1){
                     nexG[col][row] = 1;
                 }
-                else if(near > 3){
+                else if(near > 3&& board[col][row] == 1){
                     nexG[col][row] = 0;
                 }
                 else if(near == 3 && board[col][row] == 0){
